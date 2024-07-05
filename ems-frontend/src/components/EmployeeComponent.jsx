74022c00041d9fa50a6a9fdfeb9d2aaff23f4de4
SindhuK1 <sindhuk1199uk@gmail.com>
2024-07-05 20:34:38 +0530

    emssuccess

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,34 +1,83 @@
 import React, { useState } from 'react'
 import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams} from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+    
+    const {id} = useParams();
+    const [errors,setErrors] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
 
     const navigator =  useNavigate();
 
     function saveEmployee(e){
         e.preventDefault();
          
-        const employee = {firstName, lastName, email}
-        console.log(employee)
+        if(validateForm()){
+            const employee = {firstName, lastName, email}
+            console.log(employee)
 
-        createEmployee(employee).then((response)=>{
+            createEmployee(employee).then((response)=>{
             console.log(response.data);
             navigator('/employees');
-        });
+            });
+        }
+
     }
 
-   
+   function validateForm(){
+     let valid = true;
+
+     const errorsCopy = {...errors};
+
+     if(firstName.trim()){
+        errorsCopy.firstName='';
+     }else{
+        errorsCopy.firstName='First Name is required';
+        valid = false;  
+     }
+
+     if(lastName.trim()){
+        errorsCopy.lastName='';
+     }else{
+        errorsCopy.lastName='Last Name is required';
+        valid = false;  
+     }
+
+     if(email.trim()){
+        errorsCopy.email='';
+     }else{
+        errorsCopy.email='Email is required';
+        valid = false;  
+     }
+     setErrors(errorsCopy);
+
+     return valid;
+   }
+
+    function pageTitle(){
+        if(id){
+            return <h2 className='text-center'>Edit Employee</h2>
+        }else{
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
   return (
     <div className='container'>
         <br/><br/>
       <div className='row'>
         <div className='card col-md-6 offset-md-3 offset-md-3'>
-            <h2 className='text-center'>Add Employee</h2>
+            {
+                 pageTitle()
+            }
             <div className='card-body'>
                 <form>
                     <div className='form-group mb-2'>
@@ -42,6 +91,7 @@ const EmployeeComponent = () => {
                          onChange={(e) => setFirstName(e.target.value)}
                          >
                         </input>
+                        { errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
                     </div>
                     <div className='form-group mb-2'>
                         <label className='form-label'>Last Name:</label>
